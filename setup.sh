c1f4ac255ba7d0c6a9f7ea01186d3750af85bc3d
Hugo Delaunay <Skyost@users.noreply.github.com>
2023-12-12 16:39:27 +0100

    Passing the installation of `npm`.

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_version.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get -qq update
 # apt -qq -y install gcc g++ make
-apt -qq -y install nodejs npm
+apt -qq -y install nodejs
 npm install --global yarn
 node --version
 npm --version
